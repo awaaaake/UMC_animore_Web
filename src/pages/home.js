e3301b305e6d6272231affd1b1dd6f0fae2e15a9
choyunhee <xornd1110@naver.com>
2023-07-15 01:23:22 +0900

    home수정

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,16 +7,18 @@ import { Button, Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 
 function Home(props) {
     let [history, setHistory] = useState(['봉봉살롱', '도그뷰티', '까끌래보끌래']);
-    
+
     return (
         <div className="home">
             <div className="pet-profile">
-                <img src={profile} alt="반려견 사진" />
-                <div className="info">
-                    <h4>OOO님 안녕하세요!</h4>
-                    <p>반려견 : 봉봉이(포메라니안) / 5살 / 중성화O</p>
+                <div className='info2'>
+                    <img src={profile} alt="반려견 사진" />
+                    <div className="info">
+                        <h4>OOO님 안녕하세요!</h4>
+                        <p>반려견 : 봉봉이(포메라니안) / 5살 / 중성화O</p>
+                    </div>
                 </div>
-                <button className="edit-profile-button" onClick={()=> props.navigate('/profile')}>프로필 수정</button>
+                <button className="edit-profile-button" onClick={() => props.navigate('/profile')}>프로필 수정</button>
             </div>
             <div className="history">
                 <p>최근 이용 기록</p>
@@ -25,7 +27,7 @@ function Home(props) {
                         history.map(function (a, i) {
                             return (
                                 <Col sm>
-                                    <img src={grey} width="300" height="270"  onClick={() => props.navigate(`/reservation/${history[i]}`)}
+                                    <img src={grey} width="100%" height="250"  onClick={() => props.navigate(`/reservation/${history[i]}`)}
                                     ></img>
                                 </Col>
                             )
